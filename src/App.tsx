5ad7bfcde6543555439f82efc70575589e6afb28
hugo vilar <hugovilar04@gmail.com>
2024-08-07 08:48:04 -0300

    FEAT:(header) - creating header component and styling it

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Header } from "./components/Header";
+
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+      <Header />
+    </div>
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import styles from "./Header.module.css";
+import toDoLogo from "../assets/Logo.svg";
+
+export function Header() {
+    return(
+        <header className={styles.header}>
+            <img src={toDoLogo} alt="toDoLogo" />
+        </header>
+    )
+}
