22504e8b2a109d24e5ca99f6a932b61946c5c94c
hugo vilar <hugovilar04@gmail.com>
2024-08-08 08:47:41 -0300

    Adding tasks at the array

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import styles from "./App.module.css";
 
 
 function App() {
-
   return (
     <div>
       <Header />
--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,11 +1,33 @@
 import styles from "./NewTask.module.css";
 import { PlusCircle } from "phosphor-react";
+import { FormEvent, useState } from "react";
 
 export function NewTask() {
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleCreateTask(event: FormEvent) {
+    event.preventDefault();
+    if (newTask !== "") {
+      setTasks((prevTasks) => {
+        const updatedTasks = [...prevTasks, newTask];
+        return updatedTasks;
+      });
+      setNewTask("");
+    }
+  }
+
   return (
-    <div className={styles.newTask}>
-      <input type="text" placeholder="Adicionar uma nova tarefa" />
-      <button>Criar <PlusCircle size={16} /></button>
-    </div>
+    <form className={styles.newTask} onSubmit={handleCreateTask}>
+      <input
+        type="text"
+        placeholder="Adicionar uma nova tarefa"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+      />
+      <button type="submit">
+        Criar <PlusCircle size={16} />
+      </button>
+    </form>
   );
 }
