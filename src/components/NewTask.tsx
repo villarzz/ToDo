f1e2c67c136014558727c7d7592cbd1d5377991c
hugo vilar <hugovilar04@gmail.com>
2024-08-14 09:15:36 -0300

    feat

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,5 +1,6 @@
 import styles from "./NewTask.module.css";
 import { PlusCircle } from "phosphor-react";
+import Clipboard from "../assets/Clipboard.svg";
 import { FormEvent, useState } from "react";
 
 export function NewTask() {
@@ -9,22 +10,48 @@ export function NewTask() {
   function handleCreateTask(event: FormEvent) {
     event.preventDefault();
     if (newTask !== "") {
-      setTasks((prevTasks) => [...prevTasks, newTask]);
+      setTasks(prevTasks => [...prevTasks, newTask]);
       setNewTask("");
     }
   }
 
   return (
-    <form className={styles.newTask} onSubmit={handleCreateTask}>
-      <input
-        type="text"
-        placeholder="Adicionar uma nova tarefa"
-        value={newTask}
-        onChange={(e) => setNewTask(e.target.value)}
-      />
-      <button type="submit">
-        Criar <PlusCircle size={16} />
-      </button>
-    </form>
+    <div>
+      <form onSubmit={handleCreateTask} className={styles.newTask}>
+        <input
+          type="text"
+          placeholder="Adicionar uma nova tarefa"
+          value={newTask}
+          onChange={e => setNewTask(e.target.value)}
+        />
+        <button type="submit">
+          Criar <PlusCircle size={16} />
+        </button>
+      </form>
+      <div className={styles.counterWrapper}>
+        <p className={styles.tasksCreated}>
+          Tarefas criadas
+          <span className={styles.tasksCounter}>0</span>
+        </p>
+        <p className={styles.tasksCompleted}>
+          Concluidas
+          <span className={styles.tasksCounter}>0</span>
+        </p>
+      </div>
+      {/* <div className={styles.listWrapper}>
+        <div className={styles.emptyImage}>
+          <img src={Clipboard} />
+        </div>
+        <strong className={styles.emptyText}>
+          Você ainda não tem tarefas cadastradas
+        </strong>
+        <p className={styles.emptyText}>
+          Crie tarefas e organize seus itens a fazer
+        </p>
+      </div> */}
+      <div className={styles.listWrapper}>
+        
+      </div>
+    </div>
   );
 }
