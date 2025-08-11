613b6fc3c78b4ea5345022eef2f9048eb0398670
hugo vilar <hugovilar04@gmail.com>
2025-08-11 09:06:26 -0300

    FEAT(List) - remove unused imports and state management

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,13 +1,7 @@
-import styles from "./List.module.css";
-import Clipboard from "../assets/Clipboard.svg";
-import { useState } from "react";
-
 export function List() {
-    const [tasks, setComments] = useState(["Legal!"]);
-
   return (
     <div>
-      
+
     </div>
   );
 }
